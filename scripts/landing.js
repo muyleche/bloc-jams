41112dbafff83d8705a322d634b397547aadf494
Tyler BoddySpargo <muyleche@gmail.com>
2017-03-19 22:45:48 -0500

    checkpoint 8 DOM animations

---
+++ scripts/landing.js
@@ -0,0 +1,15 @@
+
+function animatePoints () {
+
+  var points = document.getElementsByClassName('point'),
+      revealPoint = function (index) {
+    points[index].style.opacity = 1;
+    points[index].style.transform = "scaleX(1) translateY(0)";
+    points[index].style.msTransform = "scaleX(1) translateY(0)";
+    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+  };
+
+  for (var i = 0; i < points.length; i++) {
+    revealPoint(i);
+  }
+};

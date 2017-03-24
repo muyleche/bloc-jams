8c2462cfea06de63df392c0d0b79e7256d777891
Tyler BoddySpargo <muyleche@gmail.com>
2017-03-23 20:48:53 -0500

    Checkpoint 9 Events: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,15 +1,31 @@
+var points = document.getElementsByClassName('point');
 
-function animatePoints () {
+function animatePoints (points) {
 
-  var points = document.getElementsByClassName('point'),
-      revealPoint = function (index) {
-    points[index].style.opacity = 1;
-    points[index].style.transform = "scaleX(1) translateY(0)";
-    points[index].style.msTransform = "scaleX(1) translateY(0)";
-    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+  function revealPoint(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
   };
 
   for (var i = 0; i < points.length; i++) {
-    revealPoint(i);
+    revealPoint(points[i]);
   }
 };
+
+window.onload = function() {
+  if (window.innerHeight > 950) {
+    animatePoints(points);
+  }
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+  var arrow = document.getElementsByClassName('arrow')[0];
+  window.addEventListener('scroll',function(event) {
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+      animatePoints(points);
+      arrow.style.opacity = 0;
+    }
+  });
+}
+
